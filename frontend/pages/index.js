da1d803c3491c0b7e497550ec914f09dc818c843
Fernando <ferry.fernando@gmail.com>
2018-11-04 16:11:30 +0700

    Custom base layout.

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,14 +1,15 @@
 import { Button, Alert } from "reactstrap";
+import Layout from '../components/Layout';
 
 export default () => {
     return (
-        <div>
+        <Layout>
             <div>
                 <Alert color="primary">
                     Hello Project is strapi-next with Bootstrap
                 </Alert>
-                &nbsp; <Button color="primary">Hello from nextjs</Button>
+                &nbsp; <Button color="primary">Hello nextjs</Button>
             </div>
-        </div>
+        </Layout>
     );
 };
